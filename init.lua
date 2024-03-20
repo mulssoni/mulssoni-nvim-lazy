25ab6d9815b6ce1126f7d657c05d2ab28756d8e5
Samuli Välimäki <samuli.valimaki@sitowise.com>
2024-03-20 09:22:50 +0200

    new plugins

--- init.lua
+++ init.lua
@@ -1,3 +1,45 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-vim.cmd.colorscheme("catppuccin")
+-- vim.cmd.colorscheme("tokyonight")
+-- local colors = require("tokyonight.colors").setup()
+vim.cmd.colorscheme("ayu-mirage")
+local colors = require("ayu.colors")
+colors.generate(true) -- Pass `true` to enable mirage
+
+require("ayu").setup({
+  overrides = {
+    Normal = { bg = "None" },
+    ColorColumn = { bg = "None" },
+    SignColumn = { bg = "None" },
+    Folded = { bg = "None" },
+    FoldColumn = { bg = "None" },
+    CursorLine = { bg = "None" },
+    CursorColumn = { bg = "None" },
+    WhichKeyFloat = { bg = "None" },
+    VertSplit = { bg = "None" },
+  },
+})
+
+require("lualine").setup({
+  options = {
+    theme = "ayu",
+  },
+})
+
+require("scrollbar").setup({
+  handle = {
+    color = colors.bg_highlight,
+  },
+  marks = {
+    Search = { color = colors.orange },
+    Error = { color = colors.error },
+    Warn = { color = colors.warning },
+    Info = { color = colors.info },
+    Hint = { color = colors.hint },
+    Misc = { color = colors.purple },
+  },
+})
+
+require("pets").setup({
+  random = false,
+})
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,1 +1,14 @@
-return { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  { "Shatur/neovim-ayu" },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+}
